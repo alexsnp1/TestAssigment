3a5d8e96c58179f6e27de9c9fd9647f55ae8c9df
Aleksandr Maliutin <alexander.hroft@gmail.com>
2025-07-26 23:56:30 +0700

    added pageObject

--- src/test/java/pages/AlfaBankPage.java
+++ src/test/java/pages/AlfaBankPage.java
@@ -5,6 +5,9 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.Keys;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class AlfaBankPage {
@@ -15,28 +18,91 @@ public class AlfaBankPage {
 
     }
 
-    public AlfaBankPage removeStringValue() {
-        $("[data-test-id='amount-input']")
-                .sendKeys(Keys.chord(Keys.COMMAND, "a"), Keys.DELETE);
+    public AlfaBankPage checkTextResult(String textResult) {
+        $$("[data-test-id='text']")
+                .findBy(Condition.text(textResult)).should(Condition.appear, Duration.ofSeconds(10)).shouldBe(visible,
+                        Duration.ofSeconds(10));
         return this;
 
     }
 
-    public AlfaBankPage ope2nPge() {
-        open("https://alfabank.ru/");
+    public AlfaBankPage selectSmallBusinessTab() {
+        $("[data-test-id='TabsHeader-mmb-title']").scrollTo().should(Condition.appear)
+                .shouldBe(visible, Duration.ofSeconds(10)).click();
         return this;
 
     }
 
-    public AlfaBankPage ope3nPae() {
-        open("https://alfabank.ru/");
+    public AlfaBankPage openBusinessRegistrationPage() {
+        $("[data-test-id='mmb_tab']").find("a[href='/sme/start/']")
+                .scrollTo().shouldBe(visible).click();
         return this;
 
     }
 
-    public AlfaBankPage Press6MonthsButton() {
+    public AlfaBankPage openSearchBar() {
+        $("[data-test-id='test-ya-button']").should(Condition.appear).shouldBe(visible).click();
+        return this;
+    }
+
+    public AlfaBankPage waitForLoadingSite() {
+        sleep(2000);
+        return this;
+        // не смог решить ни одним доступным мне способом кроме этого, буду рад новым идеям
+    }
+
+    public AlfaBankPage setValueToSearchBar() {
+        $("[data-test-id='search-input']").setValue("Дебетовая карта").pressEnter();
+        return this;
+    }
+
+    public AlfaBankPage checkIfAmountValueCorrect() {
+        $("[data-test-id='amount-input']").should(Condition.appear).shouldBe(visible).shouldHave(value("300 000"));
+        return this;
+    }
+
+    public AlfaBankPage set3MonthsDepositTerm() {
         Selenide.executeJavaScript("arguments[0].click();",
-                $$("button").findBy(Condition.text("6 месяцев")));
+                $$("button").findBy(Condition.text("3 месяца")));
+        return this;
+    }
+
+    public AlfaBankPage disableNewMoneyConditionCheckbox() {
+        $("#calculator .switch__switch_agi88").click();
+        return this;
+    }
+
+    public AlfaBankPage checkIfInterestRateCorrect() {
+        $("[data-test-id='interestRate-all']").shouldHave(text("14,50%"));
+        return this;
+    }
+
+    public AlfaBankPage clickToOpenDepositButton() {
+        $("[data-test-id='openDepositBtn-rub']").click();
+        return this;
+    }
+
+    public AlfaBankPage hoverToSmallBusinessAndIE() {
+        $("[data-test-id='test-sme']").shouldBe(visible,
+                Duration.ofSeconds(10)).hover();
+        return this;
+    }
+
+    public AlfaBankPage hoverToBusinessAccount() {
+        $$("[data-test-id='text']").
+                findBy(Condition.text("Расчётный счёт")).hover();
+        return this;
+    }
+
+    public AlfaBankPage clickToOpenAccountButton() {
+        $$("[data-test-id='text']").
+                findBy(Condition.text("Открыть счёт")).click();
+        return this;
+    }
+
+    public AlfaBankPage clickToEnglishLanguageOfSite() {
+        $("[href='https://alfabank.ru/en/']").should(Condition.appear, Duration.ofSeconds(10)).shouldBe(visible,
+                Duration.ofSeconds(10)).click();
         return this;
     }
 }
--- src/test/java/tests/SimpleTests.java
+++ src/test/java/tests/SimpleTests.java
@@ -1,68 +1,56 @@
 package tests;
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 import pages.AlfaBankPage;
-import java.time.Duration;
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.*;
+
 public class SimpleTests extends TestBase {
     AlfaBankPage alfaBankPage = new AlfaBankPage();
 
     @Test
     void shouldOpenBusinessRegistrationTab() {
-        open("https://alfabank.ru/");
-        $("[data-test-id='TabsHeader-mmb-title']").scrollTo().should(Condition.appear)
-                .shouldBe(visible, Duration.ofSeconds(10)).click();
-        $("[data-test-id='mmb_tab']").find("a[href='/sme/start/']")
-                .scrollTo().shouldBe(visible).click();
-        $$("[data-test-id='text']")
-                .findBy(Condition.text("Регистрация бизнеса онлайн")).shouldBe(visible);
+        alfaBankPage
+                .openPage()
+                .selectSmallBusinessTab()
+                .openBusinessRegistrationPage()
+                .checkTextResult("Регистрация бизнеса онлайн");
     }
 
     @Test
     void shouldSearchForDebitCardAndSeeResults() {
-        open("https://alfabank.ru/");
-        sleep(2000);
-        $("[data-test-id='test-ya-button']").should(Condition.appear).shouldBe(visible).click();
-        $("[data-test-id='search-input']").setValue("Дебетовая карта").pressEnter();
-        $$("[data-test-id='text']")
-                .findBy(Condition.text("Дебетовая карта")).should(Condition.appear,
-                        Duration.ofSeconds(10)).shouldBe(visible);
+        alfaBankPage
+                .openPage()
+                .waitForLoadingSite()
+                .openSearchBar()
+                .setValueToSearchBar()
+                .checkTextResult("Дебетовая карта");
     }
 
     @Test
     void shouldNavigateToDepositOpeningPageAfterCalculation() {
-        open("https://alfabank.ru/");
-        $("[data-test-id='amount-input']").should(Condition.appear).shouldBe(visible).shouldHave(value("300 000"));
-        Selenide.executeJavaScript("arguments[0].click();",
-                $$("button").findBy(Condition.text("3 месяца")));
-        $("#calculator .switch__switch_agi88").click();
-        $("[data-test-id='interestRate-all']").shouldHave(text("14,50%"));
-        $("[data-test-id='openDepositBtn-rub']").click();
-        $$("[data-test-id='text']").
-                findBy(Condition.text("Как открыть вклад")).shouldBe(visible);
+        alfaBankPage
+                .openPage()
+                .checkIfAmountValueCorrect()
+                .set3MonthsDepositTerm()
+                .disableNewMoneyConditionCheckbox()
+                .checkIfInterestRateCorrect()
+                .clickToOpenDepositButton()
+                .checkTextResult("Как открыть вклад");
     }
     @Test
     void shouldNavigateToBusinessAccountOpeningThroughHover () {
-        open("https://alfabank.ru/");
-        sleep(2000);
-        $("[data-test-id='test-sme']").shouldBe(visible,
-                Duration.ofSeconds(10)).hover();
-        $$("[data-test-id='text']").
-                findBy(Condition.text("Расчётный счёт")).hover();
-        $$("[data-test-id='text']").
-                findBy(Condition.text("Открыть счёт")).click();
-        $$("[data-test-id='text']").
-                findBy(Condition.text("Заявка на открытие счёта для бизнеса")).shouldBe(visible, Duration.ofSeconds(10));
+        alfaBankPage
+                .openPage()
+                .waitForLoadingSite()
+                .hoverToSmallBusinessAndIE()
+                .hoverToBusinessAccount()
+                .clickToOpenAccountButton()
+                .checkTextResult("Заявка на открытие счёта для бизнеса");
     }
     @Test
     void shouldOpenEnglishVersion () {
-        open("https://alfabank.ru/");
-        $("[href='https://alfabank.ru/en/']").should(Condition.appear, Duration.ofSeconds(10)).shouldBe(visible,
-                Duration.ofSeconds(10)).click();
-        $$("[data-test-id='text']").
-                findBy(Condition.text("The most phygital bank")).shouldBe(visible);
+        alfaBankPage
+                .openPage()
+                .clickToEnglishLanguageOfSite()
+                .checkTextResult("The most phygital bank");
     }
 
 
